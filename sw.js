f21c4ebe133616777ba7d6fb524c8914d55b3d43
Noor-Taquee <noortaquee001@gmail.com>
2026-02-08 01:11:32 +0530

    Added Icons and service worker

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'Music Player';
+const cache_version = 'v2.2.6';
+const ASSETS = [ './public/light/logo.svg' ];
+
+// Install the service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Intercept requests to serve from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
